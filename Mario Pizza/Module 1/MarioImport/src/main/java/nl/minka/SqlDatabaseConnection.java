0900b572a174a445698d305bdf58babfe48549b5
Minka <minka-96@outlook.com>
2021-10-09 23:20:41 +0200

    Can now insert Winkels into Branch and Address Tables. It checks if it already exists or not before inserting records.

--- Mario Pizza/Module 1/MarioImport/src/main/java/nl/minka/SqlDatabaseConnection.java
+++ Mario Pizza/Module 1/MarioImport/src/main/java/nl/minka/SqlDatabaseConnection.java
@@ -5,6 +5,8 @@ package nl.minka;
  */
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SqlDatabaseConnection {
 
@@ -24,32 +26,78 @@ public class SqlDatabaseConnection {
 
         try (Connection connection = DriverManager.getConnection(connectionUrl)) {
 //            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM City WHERE Id = ? ");
-            resultSet = insertBranchAddress(connection, resultSet, mb);
+            int addressId = insertBranchAddress(connection, resultSet, mb);
+            insertBranch(connection, mb, addressId);
 //
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1));
-            }
+//            while (resultSet.next()) {
+//                System.out.println(resultSet.getString(1));
+//            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-//    public ResultSet insertBranchName(Connection connection, ResultSet resultSet, String sql) throws SQLException {
-//        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Branch (Name, AddressId) VALUES (?, ?) ");
-//        preparedStatement.setString(1, sql);
-//        preparedStatement.setInt(2, 1);
-//        resultSet = preparedStatement.executeQuery();
-//        return resultSet;
-//    }
-
-    public ResultSet insertBranchAddress(Connection connection, ResultSet resultSet, MarioBranch mb) throws SQLException {
-        PreparedStatement ps = connection.prepareStatement("INSERT INTO Address (Address, PostalCode, City) VALUES (?, ?, ?)",Statement.RETURN_GENERATED_KEYS);
+    public int insertBranchAddress(Connection connection, ResultSet resultSet, MarioBranch mb) throws SQLException {
+        ResultSet rs = selectAddress(connection, mb);
+        boolean alreadyExists = rs.next();
+        if (alreadyExists) {
+            return rs.getInt(1);
+        }
+        PreparedStatement psInsert = connection.prepareStatement("INSERT INTO Address (Address, PostalCode, City) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
         String address = mb.getStreetName() + " " + mb.getRange();
-        ps.setString(1, address);
-        ps.setString(2, mb.getPostalCode());
-        ps.setString(3, mb.getCity());
-        ps.execute();
-        resultSet = ps.getGeneratedKeys();
-        return resultSet;
+        psInsert.setString(1, address);
+        psInsert.setString(2, mb.getPostalCode());
+        psInsert.setString(3, mb.getCity());
+        psInsert.execute();
+        ResultSet generatedKeys = psInsert.getGeneratedKeys();
+        return generatedKeys.getInt(1);
+    }
+
+    public void printResultSet(ResultSet resultSet) {
+        try {
+            final int columnCount = resultSet.getMetaData().getColumnCount();
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnCount; ++i) {
+                    final Object value = resultSet.getObject(i);
+                    System.out.println(value);
+                }
+                System.out.println();
+            }
+        } catch (SQLException throwables) {
+            Logger.getLogger("SQLDatabaseConnection").log(Level.WARNING, "sad", throwables);
+        }
+    }
+
+    public ResultSet selectAddress(Connection connection, MarioBranch mb) throws SQLException {
+        PreparedStatement psSelect = connection.prepareStatement("SELECT * FROM Address WHERE Address = ? AND PostalCode = ? AND City = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        String address = mb.getStreetName() + " " + mb.getRange();
+        psSelect.setString(1, address);
+        psSelect.setString(2, mb.getPostalCode());
+        psSelect.setString(3, mb.getCity());
+        ResultSet rs = psSelect.executeQuery();
+        printResultSet(rs);
+        rs.beforeFirst();
+        return rs;
+    }
+
+    public void insertBranch(Connection connection, MarioBranch mb, int addressId) throws SQLException {
+        ResultSet rs = selectBranch(connection, mb);
+        boolean alreadyExists = rs.next();
+        if (alreadyExists) {
+            return;
+        }
+        PreparedStatement psInsert = connection.prepareStatement("INSERT INTO Branch (Name, AddressId) VALUES (?,?)");
+        psInsert.setString(1, mb.getBranchName());
+        psInsert.setInt(2, addressId);
+        psInsert.execute();
+    }
+
+    public ResultSet selectBranch(Connection connection, MarioBranch mb) throws SQLException {
+        PreparedStatement psSelect = connection.prepareStatement("SELECT * FROM Branch WHERE Name = ?", ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        psSelect.setString(1, mb.getBranchName());
+        ResultSet rs = psSelect.executeQuery();
+        printResultSet(rs);
+        rs.beforeFirst();
+        return rs;
     }
 }
